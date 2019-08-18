db16f6b42bad96b497e98055d6fa492362e0358a
YuHongShanghai <34389928+YuHongShanghai@users.noreply.github.com>
2019-08-18 22:55:48 +0800

    Update tasks.py

--- orders/tasks.py
+++ orders/tasks.py
@@ -4,10 +4,6 @@ from .models import Order
 
 @task
 def order_created(order_id):
-    """
-    Task to send an e-mail notification when an order is
-    successfully created.
-    """
     order = Order.objects.get(id=order_id)
     subject = 'Order nr. {}'.format(order.id)
     message = 'Dear {},\n\nYou have successfully placed an order.\
